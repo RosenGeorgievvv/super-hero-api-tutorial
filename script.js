21dcf23d7b9fcfef6f1d05fcc68b33170ab131f8
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-01-28 15:52:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+const superHeroToken = "747964616668923";
+const baseURL = `https://superheroapi.com/api.php/${superHeroToken}`;
 
+const heroImageDiv = document.getElementById("heroImage");
 
-const getSuperHero = () => {
-    fetch('https://superheroapi.com/api.php/747964616668923/245')
-    .then(response => response.json())
-    .then(json => console.log(json.name))
-}
+const getSuperHero = (id, name) => {
+  fetch(`${baseURL}/${id}`)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      heroImageDiv.innerHTML = `<img src="${json.image.url}" height=200 width=200/>`;
+    });
+};
+//Taking the button and store it in a variable
+const newHeroButton = document.getElementById("newHeroButton");
+
+//Randomly display a hero when the button is clicked
+const randomHero = () => {
+  const numberOfHeroes = 731;
+  return Math.ceil(Math.random() * numberOfHeroes);
+};
+newHeroButton.onclick = () => getSuperHero(randomHero());
