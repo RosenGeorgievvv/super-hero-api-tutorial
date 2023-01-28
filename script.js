e4c33e86305db532a67dd76c1be872f9c552358f
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-01-28 17:41:13 +0200

    SuperHeroApp finished
    This is an example of how easily you can create app. It has got the functionality to display a random hero or you can type for exact one. For each hero there is a screenshot and description.

--- script.js
+++ script.js
@@ -1,22 +1,62 @@
 const superHeroToken = "747964616668923";
 const baseURL = `https://superheroapi.com/api.php/${superHeroToken}`;
 
-const heroImageDiv = document.getElementById("heroImage");
+const newHeroButton = document.getElementById('newHeroButton')
+
+const heroImageDiv = document.getElementById('heroImage')
+
+const searchButton = document.getElementById('searchButton')
+
+const searchInput = document.getElementById('searchInput')
 
 const getSuperHero = (id, name) => {
   fetch(`${baseURL}/${id}`)
-    .then((response) => response.json())
-    .then((json) => {
-      console.log(json);
-      heroImageDiv.innerHTML = `<img src="${json.image.url}" height=200 width=200/>`;
-    });
-};
-//Taking the button and store it in a variable
-const newHeroButton = document.getElementById("newHeroButton");
+    .then(response => response.json())
+    .then(json => {
+      console.log(json.powerstats)
+      const superHero = json
+      showHeroInfo(superHero) 
+    })
+}
 
-//Randomly display a hero when the button is clicked
+const statToEmoji = {
+  intelligence: '🧠',
+  strength: '💪',
+  speed: '⚡',
+  durability: '🏋️‍♂️',
+  power: '📊',
+  combat: '⚔️',
+}
+
+const showHeroInfo = (character) => {
+  const name = `<h2>${character.name}</h2>`
+
+  const img = `<img src="${character.image.url}" height=200 width=200/>`
+  
+  const stats = Object.keys(character.powerstats).map(stat => {
+    return `<p>${statToEmoji[stat]} ${stat.toUpperCase()}: ${character.powerstats[stat]}</p>`
+  }).join('')
+  
+  heroImageDiv.innerHTML = `${name}${img}${stats}`
+}
+
+// <p>💪 Strength: ${json.powerstats.strength}</p><p>🧠 Intelligence: ${json.powerstats.intelligence}</p><p>🧠 Combat: ${json.powerstats.intelligence}</p><p>🧠 Speed: ${json.powerstats.intelligence}</p><p>🧠 Durability: ${json.powerstats.intelligence}</p>
+
+const getSearchSuperHero = (name) => {
+  console.log(searchInput.value)
+  fetch(`${baseURL}/search/${name}`)
+    .then(response => response.json())
+    .then(json => {
+      const hero = json.results[0]
+      showHeroInfo(hero) 
+    })
+}
+
 const randomHero = () => {
-  const numberOfHeroes = 731;
-  return Math.ceil(Math.random() * numberOfHeroes);
-};
-newHeroButton.onclick = () => getSuperHero(randomHero());
+  const numberOfHeroes = 731
+  return Math.floor(Math.random() * numberOfHeroes) + 1
+}
+
+newHeroButton.onclick = () => getSuperHero(randomHero())
+
+searchButton.onclick = () => getSearchSuperHero(searchInput.value)
